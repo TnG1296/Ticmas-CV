75bd7d135534986cb83d0226a8f8c060802db224
Tomas Godoy <tomasnereogodoy@gmail.com>
2022-12-30 11:17:02 -0300

    Contenido agregado al encabezado
    El CV es de Esperanza Castillo, directora ejecutiva.
    La informacion es en parte provista por el API recomendado y otra parte la invente yo

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
 fetch ('https://randomuser.me/api/')
   .then(response => response.json())
   .then(json => console.log(json))
+
+  Usé al API para printear parte de la informacion random que completo el HTML
+*/
