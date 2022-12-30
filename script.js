a7322e0c393da4b9c61cd8714eb47975a8d791f0
Tomas Godoy <tomasnereogodoy@gmail.com>
2022-12-30 14:57:45 -0300

    Cambios CSS en seccion, comienzo con Resumen profesional

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+
+
+
+
+
+
+
 /*
 fetch ('https://randomuser.me/api/')
   .then(response => response.json())
   .then(json => console.log(json))
 
-  Usé al API para printear parte de la informacion random que completo el HTML
+  Usé al API para printear parte de la informacion random con la que complete el HTML
 */
